6fbd183cf84eaa6feffe385acf9e702ba1a1e8e8
Ariona Rian <helloariona@gmail.com>
2024-06-22 13:43:13 +0700

    Update favicon

--- service_worker.js
+++ service_worker.js
@@ -2,7 +2,7 @@
 self.addEventListener("install",e=>{
   e.waitUntil(
     caches.open("static").then(cache=>{
-      return cache.addAll(["./",'./img/icon.png']);
+      return cache.addAll(["./"]);
     })
   );
 });
